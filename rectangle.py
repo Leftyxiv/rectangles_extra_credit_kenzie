92efa4ef0007660b01b89a0ff3cedc1cd6dc7eb6
Leftyxiv <manny@aledoux.net>
2021-02-26 12:22:51 -0500

    completed with random integers

--- rectangle.py
+++ rectangle.py
@@ -1,6 +1,10 @@
+import random 
+
 tuple_list = []
-for w in range(2,10):
-  for h in range(3,10):
+wide = random.randint(2,9)
+height = random.randint(wide + 1, 10)
+for w in range(2,wide):
+  for h in range(3,height):
     tuple_list.append((w,h))
 
 sorted_list = sorted(tuple_list, key=lambda tup: (tup[0] - 1) * (tup[1] - 1))
