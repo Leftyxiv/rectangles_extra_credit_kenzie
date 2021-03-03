e016b744337cd7348e2f893428cb7a7a27d49204
Leftyxiv <manny@aledoux.net>
2021-03-03 08:02:45 -0500

    second try

--- rectangle.py
+++ rectangle.py
@@ -7,6 +7,6 @@ for w in range(2,wide):
   for h in range(3,height):
     tuple_list.append((w,h))
 
-sorted_list = sorted(tuple_list, key=lambda tup: (tup[0] - 1) * (tup[1] - 1))
+sorted_list = sorted(tuple_list, key=lambda tup: (tup[0] - 1) * (tup[1] - 1),reverse=True)
 
 print(sorted_list)
