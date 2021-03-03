cf50710f67bf65bf20be2d54d784a38db3b47829
Leftyxiv <manny@aledoux.net>
2021-03-03 09:21:42 -0500

    got it thank you so much!

--- rectangle.py
+++ rectangle.py
@@ -1,12 +1,37 @@
 import random 
 
 tuple_list = []
-wide = random.randint(2,9)
-height = random.randint(wide + 1, 10)
-for w in range(2,wide):
-  for h in range(3,height):
-    tuple_list.append((w,h))
+
+start = 9
+
+for i in range(start,1,-1):
+  for j in range(start,1,-1):
+    tuple_list.append((j,i))
+  start -= 1
+
+# for w in range(9,1,-1):
+#   for h in range(9,1,-1):
+#     tuple_list.append((w,h))
 
 sorted_list = sorted(tuple_list, key=lambda tup: (tup[0] - 1) * (tup[1] - 1),reverse=True)
 
 print(sorted_list)
+
+"""
+[(9, 9), (8, 9), (8, 8), (7, 9), (7, 8), (6, 9), (7, 7), (6, 8), (5, 9),
+ (6, 7), (5, 8), (6, 6), (4, 9), (5, 7), (4, 8), (5, 6), (4, 7), (3, 9),
+ (5, 5), (4, 6), (3, 8), (3, 7), (4, 5), (3, 6), (4, 4), (2, 9), (3, 5),
+ (2, 8), (2, 7), (3, 4), (2, 6), (2, 5), (3, 3), (2, 4), (2, 3), (2, 2)]
+"""
+
+"""
+[(9, 9), 
+ (8, 9), (8, 8), 
+ (7, 9), (7, 8), (7, 7),  
+ (6, 9), (6, 8), (6, 7), (6, 6), 
+ (5, 9), (5, 8), (5, 7), (5, 6), (5, 5),
+ (4, 9), (4, 8), (4, 7), (4, 6), (4, 4), (4, 5), 
+ (3, 9), (3, 7), (3, 6), (3, 8), (3, 5), (3, 4), (3, 3), 
+ (2, 9), (2, 8), (2, 7), (2, 6), (2, 5), (2, 4), (2, 3), (2, 2)]
+
+"""
